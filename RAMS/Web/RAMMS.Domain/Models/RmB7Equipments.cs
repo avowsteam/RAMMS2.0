66e6841bcc24631a88b70a1a52d20999719409eb
Sakthivel <sakthivel.p@avowstech.com>
2022-08-09 06:33:30 +0530

    B11 Context Added

---
+++ RAMS/Web/RAMMS.Domain/Models/RmB7Equipments.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RAMMS.Domain.Models
+{
+    public partial class RmB7Equipments
+    {
+        public RmB7Equipments()
+        {
+            RmB7EquipmentsHistory = new HashSet<RmB7EquipmentsHistory>();
+        }
+
+        public int B7ePkRefNo { get; set; }
+        public int? B7eRevisionNo { get; set; }
+        public DateTime? B7eRevisionDate { get; set; }
+        public int? B7eRevisionYear { get; set; }
+        public int? B7eCrBy { get; set; }
+        public string B7eCrByName { get; set; }
+        public DateTime? B7eCrDt { get; set; }
+
+        public virtual ICollection<RmB7EquipmentsHistory> RmB7EquipmentsHistory { get; set; }
+    }
+}
---
+++ RAMS/Web/RAMMS.Domain/Models/RmB7Material.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RAMMS.Domain.Models
+{
+    public partial class RmB7Material
+    {
+        public RmB7Material()
+        {
+            RmB7MaterialHistory = new HashSet<RmB7MaterialHistory>();
+        }
+
+        public int B7mPkRefNo { get; set; }
+        public int? B7mRevisionNo { get; set; }
+        public DateTime? B7mRevisionDate { get; set; }
+        public int? B7mRevisionYear { get; set; }
+        public int? B7mCrBy { get; set; }
+        public string B7mCrByName { get; set; }
+        public DateTime? B7mCrDt { get; set; }
+
+        public virtual ICollection<RmB7MaterialHistory> RmB7MaterialHistory { get; set; }
+    }
+}
