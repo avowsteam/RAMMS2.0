20fedac9d5ab2d68f13adb962a9cbd325f82dccc
varadharaj s <varadharaja.p@avowstech.com>
2022-08-22 19:51:43 +0530

    b9

--- RAMS/Web/RAMMS.Web.UI/wwwroot/js/FormB9.js
+++ RAMS/Web/RAMMS.Web.UI/wwwroot/js/FormB9.js
@@ -306,5 +306,5 @@ function formatDate(date) {
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
 
-    return [year, month, day].join('-');
+     [year, month, day].join('-');
 }
