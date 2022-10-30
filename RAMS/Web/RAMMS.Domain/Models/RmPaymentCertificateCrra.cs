12ae96f7d3dc684bf416c3ba355ab51f93f69bff
Sakthivel <sakthivel.p@avowstech.com>
2022-10-30 17:36:50 +0530

    fixed

---
+++ RAMS/Web/RAMMS.Domain/Models/RmPaymentCertificateCrra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RAMMS.Domain.Models
+{
+    public partial class RmPaymentCertificateCrra
+    {
+        public int CrraPkRefNo { get; set; }
+        public int? CrraPcmamwPkRefNo { get; set; }
+        public string CrraDescription { get; set; }
+        public decimal? CrraThisPayment { get; set; }
+        public decimal? CrraTillLastPayment { get; set; }
+        public decimal? CrraTotalToDate { get; set; }
+
+        public virtual RmPaymentCertificateMamw CrraPcmamwPkRefNoNavigation { get; set; }
+    }
+}
