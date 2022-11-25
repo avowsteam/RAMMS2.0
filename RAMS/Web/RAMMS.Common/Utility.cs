cc3770a6120b0ef2387c0495ff0a08114337c307
nagul <nagulmeera.s@avowstech1.onmicrosoft.com>
2022-11-25 11:33:15 +0530

    DLP changes

--- RAMS/Web/RAMMS.Common/Utility.cs
+++ RAMS/Web/RAMMS.Common/Utility.cs
@@ -324,17 +324,17 @@ namespace RAMMS.Common
                 case 3:
                     return "MAC";
                 case 4:
-                    return "APRIL";
+                    return "APR";
                 case 5:
                     return "MAY";
                 case 6:
-                    return "JUNE";
+                    return "JUN";
                 case 7:
                     return "JUL";
                 case 8:
                     return "AUG";
                 case 9:
-                    return "SEPT";
+                    return "SEP";
                 case 10:
                     return "OCT";
                 case 11:
